7d3ad00a2fe8c6e120ec0615bda55042cc7ebfcd
Edward Opare-Aryee <ed@thisissoon.com>
2015-03-16 16:11:12 +0000

    Add snTitle service
    Allows site title to be set in javascript and page title to be set after angular app config

--- tests/unit/title.js
+++ tests/unit/title.js
@@ -1,6 +1,6 @@
 "use strict";
 
-describe("sn.title:title", function() {
+describe("sn.title:title directive", function() {
     var element, $scope, $rootScope, errorText;
 
     beforeEach(module("sn.title"));
@@ -77,6 +77,64 @@ describe("sn.title:title", function() {
         it("should render directive with error title text", function(){
             $rootScope.$broadcast("$routeChangeError")
             expect(element.html()).toEqual(errorText);
+        });
+
+    });
+
+});
+
+describe("sn.title:snTitle service", function() {
+    var element, $scope, $rootScope, errorText, snTitle;
+
+    beforeEach(module("sn.title", function (snTitleProvider){
+        snTitleProvider.setSiteTitle("My Site Name");
+    }));
+
+    beforeEach(inject(function (_$rootScope_, $compile, $injector) {
+        $rootScope = _$rootScope_;
+
+        spyOn($rootScope, "$broadcast").and.callThrough();
+
+        $scope = $rootScope.$new();
+
+        snTitle = $injector.get("snTitle");
+
+        errorText = $injector.get("ROUTE_CHANGE_ERROR_TITLE")
+
+        element = "<title></title>";
+
+        element = $compile(element)($scope);
+        $scope.$digest();
+
+    }));
+
+    describe("site title defined using snTitle service", function() {
+
+        it("should render directive with correct title text", function(){
+            $rootScope.$broadcast("$routeChangeSuccess", {
+                $$route: {
+                    title: "foo"
+                }
+            })
+            expect(element.html()).toEqual("foo - My Site Name");
+
+            $rootScope.$broadcast("$routeChangeSuccess", {
+                $$route: {
+                    title: undefined
+                }
+            })
+            expect(element.html()).toEqual("My Site Name");
+        });
+
+        it("should render directive with error title text", function(){
+            $rootScope.$broadcast("$routeChangeError")
+            expect(element.html()).toEqual(errorText + " - My Site Name");
+        });
+
+        it("should update title element text with page title", function(){
+            snTitle.setPageTitle("My page");
+            expect($scope.$broadcast).toHaveBeenCalled();
+            expect(element.html()).toEqual("My page - My Site Name");
         });
 
     });

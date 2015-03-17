dcf54b239e3b00c1edd231a781fd641bbf95ff33
Edward Opare-Aryee <ed@thisissoon.com>
2015-03-17 09:52:41 +0000

    Release v0.0.2

--- dist/angular-title.js
+++ dist/angular-title.js
@@ -1,4 +1,4 @@
-/*! angular-title - v0.0.1 - 2015-03-16 */
+/*! angular-title - v0.0.2 - 2015-03-17 */
 "use strict";
 /**
  * Angular Title dyamically updates the document title when navigating views
@@ -13,65 +13,107 @@
  * `My Site Name`.
  *
  * @example
- *  $routeProvider
- *      .when("/pageone", {
- *          controller: "pageoneCtrl"
- *          title: "pageone",
- *          templateUrl: "partials/pageone.html"
- *      })
- *      .when("/pagetwo", {
- *          controller: "pagetwoCtrl"
- *          title: "pagetwo",
- *          templateUrl: "partials/pagetwo.html"
- *      })
+    $routeProvider
+        .when("/pageone", {
+            controller: "pageoneCtrl"
+            title: "pageone",
+            templateUrl: "partials/pageone.html"
+        })
+        .when("/pagetwo", {
+            controller: "pagetwoCtrl"
+            title: "pagetwo",
+            templateUrl: "partials/pagetwo.html"
+        })
  * @main   sn.title
  * @module sn.title
  * @author SOON_
  */
 angular.module("sn.title", [])
-
 /**
  * Title text to display when $routeChangeError
  * event occurs.
  * @constant
  * @property ROUTE_CHANGE_ERROR_TITLE
- * @type     {String}
+ * @type {String}
  */
 .constant("ROUTE_CHANGE_ERROR_TITLE", "Page Error")
-
+/**
+ * @constant
+ * @property EVENTS
+ * @type {Object}
+ */
+.constant("EVENTS", {
+    SET_TITLE: "sn.title:setTitle",
+    ROUTE_CHANGE_SUCCESS: "$routeChangeSuccess",
+    ROUTE_CHANGE_ERROR: "$routeChangeError",
+})
 /**
  * Title element directive which updates it's content to
  * update the page title. Place the name of you site inside
  * the directive and it will be appended to the end of every
  * page title. eg. Page Title - My Site Name
  * @example
- *   <title>My Site Name</title>
+     <title>My Site Name</title>
  * @class  title
  * @module sn.title
  * @author SOON_
  */
 .directive("title", [
     "$rootScope",
+    "snTitle",
+    "EVENTS",
     "ROUTE_CHANGE_ERROR_TITLE",
     /**
      * @constructor
      * @param {Service} $rootScope
+     * @param {Service} snTitle
+     * @param {String}  EVENTS
      * @param {String}  ROUTE_CHANGE_ERROR_TITLE
      */
-    function ($rootScope, ROUTE_CHANGE_ERROR_TITLE) {
+    function ($rootScope, snTitle, EVENTS, ROUTE_CHANGE_ERROR_TITLE) {
         return {
             restrict: "E",
             link: function ($scope, $element) {
 
                 /**
-                 * The name of the site to use to append
-                 * to all page titles.
+                 * The name of the site. We use this to append to all page titles.
+                 * We use various sources to get the site title, here is the list
+                 * in order of priority:
+                 *   1. First attempt to get the site title from snTitle service
+                 *   2. Fallback to text inside <title> element for site title
+                 *   3. Set title as undefined
                  * @property siteTitle
-                 * @type     {String}
+                 * @type {String}
                  * @example
-                 *  My Site Name
+                    "My Site Name"
                  */
-                var siteTitle = $element.html().length > 0 ? $element.html() : undefined ;
+                var siteTitle =
+                    snTitle.getSiteTitle() && snTitle.getSiteTitle().length > 0 ?
+                        snTitle.getSiteTitle() :
+                        ($element.html().length > 0 ? $element.html() : undefined) ;
+
+                /**
+                 * Update the content of the title element to the value
+                 * of the title key in the object of the current route
+                 * @method setTitle
+                 * @param  {Event}  $event    Angular event object
+                 * @param  {String} pageTitle Value to set the document title to
+                 */
+                var setTitle = function setTitle($event, pageTitle){
+
+                    // route title & site title
+                    if (pageTitle && siteTitle){
+                        $element.html(pageTitle + " - " + siteTitle);
+
+                    // route title only
+                    } else if (pageTitle){
+                        $element.html(pageTitle);
+
+                    // site title only
+                    } else if (siteTitle){
+                        $element.html(siteTitle);
+                    }
+                };
 
                 /**
                  * Update the content of the title element to the value
@@ -82,18 +124,13 @@ angular.module("sn.title", [])
                  */
                 var onRouteChangeSuccess = function onRouteChangeSuccess($event, current){
 
-                    // route title & site title
-                    if (current && current.$$route && current.$$route.title && siteTitle){
-                        $element.html(current.$$route.title + " - " + siteTitle);
+                    var pageTitle = null;
 
-                    // route title only
-                    } else if (current && current.$$route && current.$$route.title){
-                        $element.html(current.$$route.title);
-
-                    // site title only
-                    } else if (siteTitle){
-                        $element.html(siteTitle);
+                    if (current && current.$$route && current.$$route.title){
+                        pageTitle = current.$$route.title;
                     }
+
+                    setTitle($event, pageTitle);
                 };
 
                 /**
@@ -110,10 +147,76 @@ angular.module("sn.title", [])
                     }
                 };
 
-                $rootScope.$on("$routeChangeSuccess", onRouteChangeSuccess);
-                $rootScope.$on("$routeChangeError", onRouteChangeError);
+                $rootScope.$on(EVENTS.SET_TITLE, setTitle);
+
+                $rootScope.$on(EVENTS.ROUTE_CHANGE_SUCCESS, onRouteChangeSuccess);
+                $rootScope.$on(EVENTS.ROUTE_CHANGE_ERROR, onRouteChangeError);
 
             }
         };
     }
-]);
+])
+/**
+ * Service that sets the title of the document by specifying
+ * the page title and the site title.
+ * @example
+     angular.module("myApp", ["sn.title"])
+       .config([
+         "snTitleProvider",
+         function(snTitleProvider){
+           snTitleProvider.setSiteTitle("My Site Name");
+         }
+       ])
+       .controller("myCtrl",[
+         "snTitle",
+         function (snTitle){
+           snTitle.setPageTitle("My Page");
+         }
+       ])
+ * @class  snTitle
+ * @module sn.title
+ * @author SOON_
+ */
+.provider("snTitle", function() {
+    /**
+     * @property siteTitle
+     * @type {String}
+     */
+    var siteTitle = null;
+    /**
+     * @method setSiteTitle
+     * @param {String} value Value to set the site title to
+     */
+    this.setSiteTitle = function setSiteTitle(value) {
+        siteTitle = value;
+    };
+    /**
+     * @property $get
+     * @param {Array}
+     */
+    this.$get = [
+        "$rootScope",
+        "EVENTS",
+        function ($rootScope, EVENTS) {
+            return {
+                /**
+                 * @method getSiteTitle
+                 * @return {String} Value of the site title
+                 */
+                getSiteTitle: function getSiteTitle(){
+                    return siteTitle;
+                },
+                /**
+                 * Set the page title of title directive.
+                 * Broadcasts an event which is being listen
+                 * to by title directive
+                 * @method setPageTitle
+                 * @param {String} value Value to set the current page title to
+                 */
+                setPageTitle: function setPageTitle(value){
+                    $rootScope.$broadcast(EVENTS.SET_TITLE, value);
+                }
+            };
+        }
+    ];
+});
